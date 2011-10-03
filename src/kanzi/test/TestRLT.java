24a405bc6190fa77ddd1e775ab706cea31e6e41b
fred <fred@PC>
2011-10-03 13:40:00 -0700

    Report timings for encoding and decoding separately

--- src/kanzi/test/TestRLT.java
+++ src/kanzi/test/TestRLT.java
@@ -166,9 +166,14 @@ public class TestRLT
          }
 
          RLT rlt = new RLT();
-         long before = System.nanoTime();
+         long before, after;
+         long delta1 = 0;
+         long delta2 = 0;
+         int iter = 100000;
 
-         for (int ii = 0; ii < 100000; ii++)
+         before = System.nanoTime();
+
+         for (int ii = 0; ii < iter; ii++)
          {
             rlt = new RLT(); // Required to reset internal attributes
             iba1.index = 0;
@@ -176,7 +181,11 @@ public class TestRLT
             rlt.forward(iba1, iba2);
          }
 
-         for (int ii = 0; ii < 100000; ii++)
+         after = System.nanoTime();
+         delta1 += (after - before);
+         before = System.nanoTime();
+
+         for (int ii = 0; ii < iter; ii++)
          {
             rlt = new RLT(); // Required to reset internal attributes
             iba1.index = 0;
@@ -184,8 +193,11 @@ public class TestRLT
             rlt.inverse(iba2, iba1);
          }
 
-         long after = System.nanoTime();
-         System.out.println("Elapsed [ms]: " + (after - before) / 1000000);
+         after = System.nanoTime();
+         delta2 += (after - before);
+         System.out.println("Iterations: "+iter);
+         System.out.println("RLT encoding [ms]: " + delta1 / 1000000);
+         System.out.println("RLT decoding [ms]: " + delta2 / 1000000);
       }
    }
 }
