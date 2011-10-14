e47dc654873af0e4f7760c10326c10ebc93abd2e
fred <fred@PC>
2011-10-13 22:46:29 -0700

    Add support for command line arguments

--- src/kanzi/test/TestContextResizer.java
+++ src/kanzi/test/TestContextResizer.java
@@ -34,12 +34,109 @@ public class TestContextResizer
     {
         try
         {
-            String fileName = (args.length > 0) ? args[0] : "c:\\temp\\lena.jpg";
+            String fileName = "c:\\temp\\lena.jpg";
+            boolean debug = false;
+            boolean vertical = false;
+            boolean horizontal = false;
+            boolean speed = false;
+            int effectPct = 10;
+            boolean fileProvided = false;
+
+            for (String arg : args)
+            {
+               arg = arg.trim();
+               
+               if (arg.equals("-help"))
+               {
+                   System.out.println("-help               : display this message");
+                   System.out.println("-debug              : display the computed geodesics");
+                   System.out.println("-strength=<percent> : number of geodesics to create (in percent of dimension)");
+                   System.out.println("-vertical           : process vertical geodesics");
+                   System.out.println("-horizontal         : process horizontal geodesics");
+                   System.out.println("-speedtest          : run am extra speed test");
+                   System.exit(0);
+               }
+               else if (arg.equals("-debug"))
+               {
+                   debug = true;
+                   System.out.println("Debug set to true");
+               }
+               else if (arg.startsWith("-file="))
+               {
+                  fileName = arg.substring(6);
+                  fileProvided = true;
+               }
+               else if (arg.equals("-vertical"))
+               {
+                   vertical = true;
+                   System.out.println("Vertical set to true");
+               }
+               else if (arg.equals("-horizontal"))
+               {
+                   horizontal = true;
+                   System.out.println("Horizontal set to true");
+               }
+               else if (arg.startsWith("-strength="))
+               {
+                  arg = arg.substring(10);
+                  
+                  try
+                  {
+                     int pct = Integer.parseInt(arg);
+                     
+                     if (pct < 1)
+                     {
+                         System.err.println("The minimum strength is 1%, the provided value is "+arg);
+                         System.exit(1);
+                     }
+                     else if (pct > 90)
+                     {
+                         System.err.println("The maximum strength is 90%, the provided value is  "+arg);
+                         System.exit(1);
+                     }
+                     else
+                         effectPct = pct;                     
+                  }
+                  catch (NumberFormatException e)
+                  {
+                     System.err.println("Invalid effect strength (percentage) provided on command line: "+arg);
+                  }
+               }
+               else if (arg.equals("-speedtest"))
+               {
+                   speed = true;
+                   System.out.println("Speed test set to true");
+               }
+               else
+               {
+                   System.out.println("Warning: unknown option: ["+ arg + "]");
+               }
+            }
+           
+            if ((vertical == false) && (horizontal == false))
+            {
+               System.out.println("Warning: no direction has been selected, selecting both");
+               vertical = true;
+               horizontal = true;
+            }
+
+            if (fileProvided == false)
+                System.out.println("No image file name provided on command line, using default value");
+
+            System.out.println("File name set to '" + fileName + "'");
+            System.out.println("Stength set to "+effectPct+"%");
             ImageIcon icon = new ImageIcon(fileName);
             Image image = icon.getImage();
             int w = image.getWidth(null);
             int h = image.getHeight(null);
-            System.out.println(w+"x"+h);
+            
+            if ((w<0) || (h<0))
+            {
+                System.err.println("Cannot read or decode input file '"+fileName+"'");
+                System.exit(1);
+            }
+            
+            System.out.println("Image dimensions: "+w+"x"+h);
             GraphicsDevice gs = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
             GraphicsConfiguration gc = gs.getDefaultConfiguration();
             BufferedImage img = gc.createCompatibleImage(w, h, Transparency.OPAQUE);
@@ -56,112 +153,47 @@ public class TestContextResizer
             img.getRaster().getDataElements(0, 0, w, h, src);
             ContextResizer effect;
 
-
-            // Vertical Filter
             Arrays.fill(dst, 0);
-            int dir = ContextResizer.VERTICAL;
-            int diff = (dir == ContextResizer.VERTICAL) ? (w * 8) / 100 : (h * 8) / 100;
+            int dir = 0;
+            
+            if (vertical == true) 
+                dir |= ContextResizer.VERTICAL;
+            
+            if (horizontal == true)
+                dir |= ContextResizer.HORIZONTAL;
+            
+            int min = Math.min(h, w);
             effect = new ContextResizer(w, h, 0, w, dir,
-                    ContextResizer.SHRINK,
-                    (dir == ContextResizer.VERTICAL) ? w : h, diff);//(w*10)/100);
+                    ContextResizer.SHRINK, min, min * effectPct / 100);            
+            effect.setDebug(debug);
             effect.apply(src, dst);
 
             BufferedImage img2 = gc.createCompatibleImage(w, h, Transparency.OPAQUE);
             img2.getRaster().setDataElements(0, 0, w, h, dst);
-            JFrame frame2 = new JFrame("Filter - Vertical");
+            JFrame frame2 = new JFrame("Filter");
             frame2.setBounds(500, 80, w, h);
             ImageIcon icon2 = new ImageIcon(img2);
             frame2.add(new JLabel(icon2));
             frame2.setVisible(true);
 
-
-            // Vertical Filter with debug
-            Arrays.fill(dst, 0);
-            img.getRaster().getDataElements(0, 0, w, h, src);
-            effect = new ContextResizer(w, h, 0, w, dir,
-                    ContextResizer.SHRINK,
-                    (dir == ContextResizer.VERTICAL) ? w : h, diff);//(w*10)/100);
-            effect.setDebug(true);
-            effect.apply(src, dst);
-            BufferedImage img3 = gc.createCompatibleImage(w, h, Transparency.OPAQUE);
-            img3.getRaster().setDataElements(0, 0, w, h, dst);
-            JFrame frame3 = new JFrame("Filter - Vertical - Debug");
-            frame3.setBounds(950, 110, w, h);
-            ImageIcon icon3 = new ImageIcon(img3);
-            frame3.add(new JLabel(icon3));
-            frame3.setVisible(true);
-
-
-            // Horizontal Filter
-            Arrays.fill(dst, 0);
-            img.getRaster().getDataElements(0, 0, w, h, src);
-            dir = ContextResizer.HORIZONTAL;
-            diff = (dir == ContextResizer.VERTICAL) ? (w * 8) / 100 : (h * 8) / 100;
-            effect = new ContextResizer(w, h, 0, w, dir,
-                    ContextResizer.SHRINK,
-                    (dir == ContextResizer.VERTICAL) ? w : h, diff);//(w*10)/100);
-            effect.apply(src, dst);
-
-            BufferedImage img4 = gc.createCompatibleImage(w, h, Transparency.OPAQUE);
-            img4.getRaster().setDataElements(0, 0, w, h, dst);
-            JFrame frame4 = new JFrame("Filter - Horizontal");
-            frame4.setBounds(500, 480, w, h);
-            ImageIcon icon4 = new ImageIcon(img4);
-            frame4.add(new JLabel(icon4));
-            frame4.setVisible(true);
-
-
-            // Horizontal Filter with debug
-            Arrays.fill(dst, 0);
-            img.getRaster().getDataElements(0, 0, w, h, src);
-            effect = new ContextResizer(w, h, 0, w, dir,
-                    ContextResizer.SHRINK,
-                    (dir == ContextResizer.VERTICAL) ? w : h, diff);//(w*10)/100);
-            effect.setDebug(true);
-            effect.apply(src, dst);
-            BufferedImage img5 = gc.createCompatibleImage(w, h, Transparency.OPAQUE);
-            img5.getRaster().setDataElements(0, 0, w, h, dst);
-            JFrame frame5 = new JFrame("Filter - Horizontal - Debug");
-            frame5.setBounds(950, 500, w, h);
-            ImageIcon icon5 = new ImageIcon(img5);
-            frame5.add(new JLabel(icon5));
-            frame5.setVisible(true);
-
-
-            // Vertical and Horizontal Filter
-            Arrays.fill(dst, 0);
-            dir = ContextResizer.VERTICAL | ContextResizer.HORIZONTAL;
-            diff = (dir == ContextResizer.VERTICAL) ? (w * 8) / 100 : (h * 8) / 100;
-            effect = new ContextResizer(w, h, 0, w, dir,
-                    ContextResizer.SHRINK,
-                    (dir == ContextResizer.VERTICAL) ? w : h, diff);//(w*10)/100);
-            effect.apply(src, dst);
-            BufferedImage img6 = gc.createCompatibleImage(w, h, Transparency.OPAQUE);
-            img6.getRaster().setDataElements(0, 0, w, effect.getHeight(), dst);
-            JFrame frame6 = new JFrame("Filter - Vertical & Horizontal");
-            frame6.setBounds(50, 500, w, h);
-            ImageIcon icon6 = new ImageIcon(img6);
-            frame6.add(new JLabel(icon6));
-            frame6.setVisible(true);
-
-
             // Speed test
+            if (speed == true)
             {
                 System.out.println("Speed test");
-                long before = System.nanoTime();
                 long sum = 0;
                 int iter = 1000;
 
                 for (int ii=0; ii<iter; ii++)
                 {
-                   effect = new ContextResizer(w, h, 0, w, ContextResizer.VERTICAL,
-                        ContextResizer.SHRINK, w, (w*5)/100);
-
+                   img.getRaster().getDataElements(0, 0, w, h, src);
+                   long before = System.nanoTime();
+                   effect = new ContextResizer(w, h, 0, w, dir,
+                        ContextResizer.SHRINK, min, min * effectPct/100);
                    effect.apply(src, dst);
+                   long after = System.nanoTime();
+                   sum += (after - before);
                 }
 
-                long after = System.nanoTime();
-                sum += (after - before);
                 System.out.println("Elapsed [ms]: "+ sum/1000000+" ("+iter+" iterations)");
             }
 
