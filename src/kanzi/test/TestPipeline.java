7e3fad53a0a6db2fd74a4be8f1488a8844a41bd1
fred <fred@PC>
2011-11-16 16:12:35 -0800

    Add command line arguments Allow non square input images

--- src/kanzi/test/TestPipeline.java
+++ src/kanzi/test/TestPipeline.java
@@ -46,23 +46,77 @@ import kanzi.util.ImageQualityMonitor;
 
 public class TestPipeline
 {
-   static class Entry
-   {
-      Entry prev;
-      Integer number;
-
-      Entry (Entry e, int n)
-      {
-         prev = e;
-         number = n;
-      }
-   }
+    public static final int Q0 = 310;
+    public static final int Q1 = 40;
 
     public static void main(String[] args)
     {
         try
         {
-            String fileName = "C:\\temp\\lena.jpg";
+            int blockSize = 100000;
+            boolean debug = true;
+            String fileName = "c:\\temp\\lena.jpg";
+            boolean fileProvided = false;
+
+            for (String arg : args)
+            {
+               arg = arg.trim();
+               
+               if (arg.equals("-help"))
+               {
+                   System.out.println("-help             : display this message");
+                   System.out.println("-debug            : display debug information");
+                   System.out.println("-file=<filename>  : name of the input file to encode or decode");
+                   System.out.println("-block=<size>     : size of the block (max 16 MB)");
+                   System.exit(0);
+               }
+               else if (arg.equals("-debug"))
+               {
+                   debug = true;
+                   System.out.println("Debug set to true");
+               }
+               else if (arg.startsWith("-file="))
+               {
+                  fileName = arg.substring(6);
+                  fileProvided = true;
+               }
+               else if (arg.startsWith("-block="))
+               {
+                  arg = arg.substring(7);
+                  
+                  try
+                  {
+                     int blksz = Integer.parseInt(arg);
+                     
+                     if (blksz < 256)
+                     {
+                         System.err.println("The minimum block size is 256, the provided value is "+arg);
+                         System.exit(1);
+                     }
+                     else if (blksz > 16 * 1024 * 1024 - 7)
+                     {
+                         System.err.println("The maximum block size is 16777209, the provided value is  "+arg);
+                         System.exit(1);
+                     }
+                     else
+                         blockSize = blksz;                     
+                  }
+                  catch (NumberFormatException e)
+                  {
+                     System.err.println("Invalid block size provided on command line: "+arg);
+                  }
+               }
+               else
+               {
+                   System.out.println("Warning: unknown option: ["+ arg + "]");
+               }
+            }
+            
+            if (fileProvided == false)
+                System.out.println("No input file name provided on command line, using default value");
+            
+            System.out.println("Input file name set to '" + fileName + "'");
+            System.out.println("Block size set to "+blockSize);
             ImageIcon icon = new ImageIcon(fileName);
             Image image = icon.getImage();
             int w = image.getWidth(null);
@@ -70,12 +124,10 @@ public class TestPipeline
             System.out.println(w + "x" + h);
 
 
-            if (w != h)
+            if ((w < 0) || (h < 0))
             {
-                System.err.println("Width and height must be equal");
-                w = h;
-                System.err.println("Resizing to "+w+"x"+h);
-                //System.exit(1);
+                System.err.println("Could not load image '" + fileName +"'");
+                System.exit(1);
             }
 
             GraphicsDevice gs = GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
@@ -93,12 +145,13 @@ public class TestPipeline
             // img.getRaster().getDataElements()
             img.getRaster().getDataElements(0, 0, w, h, rgb);
 
-            encode(fileName, rgb, y, u, v, w, h);
-//System.exit(0);
-            decode(fileName, rgb2, y, u, v, w, h);
+            encode(fileName, rgb, y, u, v, w, h, debug, blockSize);
+            decode(fileName, rgb2, y, u, v, w, h, debug, blockSize);
 
             int psnr1024 = new ImageQualityMonitor(w, h).computePSNR(rgb, rgb2);
             System.out.println("PSNR: "+(float) psnr1024 / 1024);
+            int ssim1024 = new ImageQualityMonitor(w, h).computeSSIM(rgb, rgb2);
+            System.out.println("SSIM: "+(float) ssim1024 / 1024);
 
             // Do NOT use img.setRGB(): it is more than 10 times slower than
             // img.getRaster().setDataElements()
@@ -129,26 +182,24 @@ public class TestPipeline
         }
         catch (Exception e)
         {
-            e.printStackTrace();
+           e.printStackTrace();
         }
     }
 
 
-    private static void encode(String fileName, int[] rgb, int[] y, int[] u, int[] v, int w, int h) throws Exception
+    private static void encode(String fileName, int[] rgb, int[] y, int[] u, int[] v, 
+            int w, int h, boolean debug, int blockSize) throws Exception
     {
         File output = new File(fileName.substring(0, fileName.length()-3).concat("bin"));
         FileOutputStream fos = new FileOutputStream(output);
         BitStream dbs = new DefaultBitStream(fos, 16384);
 
-        int dimImage = h;
-        DWT_CDF_9_7 ydwt = new DWT_CDF_9_7(dimImage);
-        DWT_CDF_9_7 uvdwt = new DWT_CDF_9_7(dimImage/2);
+        DWT_CDF_9_7 ydwt = new DWT_CDF_9_7(w, h);
+        DWT_CDF_9_7 uvdwt = new DWT_CDF_9_7(w/2, h/2);
         EntropyEncoder entropyEncoder = new RangeEncoder(dbs);
-//        Resampler rs = new BilinearResampler(w, h, 2);
-//        DefaultColorModelConverter cvt = new DefaultColorModelConverter(w, h, rs);
         YSbSrColorModelConverter cvt = new YSbSrColorModelConverter(w, h);
         long before = System.nanoTime();
-        int iter = 100;
+        int iter = 1;
 
         for (int ii=0; ii<iter; ii++)
         {
@@ -156,42 +207,42 @@ public class TestPipeline
             cvt.convertRGBtoYUV(rgb, y, u, v, ColorModelType.YUV420);
 
             // Discrete Wavelet Transform
-            encode_(dbs, y, ydwt, entropyEncoder);
-            encode_(dbs, u, uvdwt, entropyEncoder);
-            encode_(dbs, v, uvdwt, entropyEncoder);
+            encode_(y, ydwt, entropyEncoder, blockSize);
+            encode_(u, uvdwt, entropyEncoder, blockSize);
+            encode_(v, uvdwt, entropyEncoder, blockSize);
         }
 
         entropyEncoder.dispose();
         dbs.close();
         long after = System.nanoTime();
-        System.out.println("Encoding time [ms]: "+(after-before)/1000000);
-        System.out.println("Read: "+(w*h*3*iter));
-        System.out.println("Written: "+(dbs.written() >> 3));
+        
+        if (debug == true)
+        {
+          System.out.println("Encoding time [ms]: "+(after-before)/(iter*1000000));
+          System.out.println("Read: "+(w*h*3*iter));
+          System.out.println("Written: "+(dbs.written() >> 3));
+        }
+        
         float r = (float) (w*h*3*iter) / (dbs.written() >> 3);
         System.out.println("Compression ratio: "+ r);
     }
 
 
-     private static void encode_(BitStream dbs, int[] data, DWT_CDF_9_7 dwt, EntropyEncoder encoder)
+     private static void encode_(int[] data, DWT_CDF_9_7 dwt, EntropyEncoder encoder, int blockSize)
      {
-        int DIM_BAND_LL = dwt.getDimensionBandLL();
-        int levels = 0;
-        int dimImage = dwt.getDimension();
-
-        for (int dim=DIM_BAND_LL; dim<dimImage; dim<<=1)
-            levels++;
+        int levels = dwt.getLevels();
 
         // Perform Discrete Wavelet Transform band by band
         dwt.forward(data, 0);
 
         IndexedIntArray source = new IndexedIntArray(data, 0);
-        int[] buffer = new int[dimImage*dimImage];
+        int[] buffer = new int[dwt.getWidth()*dwt.getHeight()];
         IndexedIntArray destination = new IndexedIntArray(buffer, 0);
 
         // Quantization
         int[] quantizers = new int[levels+1];
-        quantizers[0] = 310;//169;
-        quantizers[1] = 40;//26;
+        quantizers[0] = Q0;
+        quantizers[1] = Q1;
 
         for (int i=2; i<quantizers.length; i++)
         {
@@ -201,8 +252,8 @@ public class TestPipeline
 
 //        WaveletRateDistorsionFilter filter = new WaveletRateDistorsionFilter(
 //                dimImage, levels, 100, dimImage, true);
-        WaveletBandFilter filter = new WaveletBandFilter(dimImage,
-                DIM_BAND_LL, levels, quantizers);
+        WaveletBandFilter filter = new WaveletBandFilter(dwt.getWidth(),
+                dwt.getHeight(), levels, quantizers);
 
         // The filter guarantees that all coefficients have been shrunk to byte values
         filter.forward(source, destination);
@@ -219,7 +270,7 @@ public class TestPipeline
 
         while (length > 0)
         {
-            int blkSize = (length < 65535) ? length : 65535;
+            int blkSize = (length < blockSize) ? length : blockSize;
             bc.setSize(blkSize);
             int encoded = bc.encode(block, encoder);
 
@@ -232,34 +283,30 @@ public class TestPipeline
             length -= blkSize;
         }
 
-        // End of block: add empty block 0 0 0 or 0x80
-        encoder.encodeByte((byte) 0); // mode
-        encoder.encodeByte((byte) 0); // MSB size
-        encoder.encodeByte((byte) 0); // LSB size
+        // End of block: add empty block: 0x80
+        encoder.encodeByte((byte) 0x80);
     }
 
 
-    private static void decode(String fileName, int[] rgb, int[] y, int[] u, int[] v, int w, int h) throws Exception
+    private static void decode(String fileName, int[] rgb, int[] y, int[] u, int[] v, 
+            int w, int h, boolean debug, int blockSize) throws Exception
     {
         File input = new File(fileName.substring(0, fileName.length()-3).concat("bin"));
         FileInputStream is = new FileInputStream(input);
         BitStream dbs = new DefaultBitStream(is, 16384);
-
-        int dimImage = h;
-        DWT_CDF_9_7 ydwt = new DWT_CDF_9_7(dimImage);
-        DWT_CDF_9_7 uvdwt = new DWT_CDF_9_7(dimImage/2);
+        DWT_CDF_9_7 ydwt = new DWT_CDF_9_7(w, h);
+        DWT_CDF_9_7 uvdwt = new DWT_CDF_9_7(w/2, h/2);
         EntropyDecoder entropyDecoder = new RangeDecoder(dbs);
-
         YSbSrColorModelConverter cvt = new YSbSrColorModelConverter(w, h);
         long before = System.nanoTime();
-        int iter = 100;
+        int iter = 1;
 
         for (int ii=0; ii<iter; ii++)
         {
             // Discrete Wavelet Inverse Transform
-            decode_(dbs, y, ydwt, entropyDecoder);
-            decode_(dbs, u, uvdwt, entropyDecoder);
-            decode_(dbs, v, uvdwt, entropyDecoder);
+            decode_(y, ydwt, entropyDecoder, blockSize);
+            decode_(u, uvdwt, entropyDecoder, blockSize);
+            decode_(v, uvdwt, entropyDecoder, blockSize);
 
             // Color space conversion
             cvt.convertYUVtoRGB(y, u, v, rgb, ColorModelType.YUV420);
@@ -268,17 +315,20 @@ public class TestPipeline
         long after = System.nanoTime();
         entropyDecoder.dispose();
         dbs.close();
-        System.out.println("Decoding time [ms]: "+(after-before)/1000000);
-        System.out.println("Read: "+(dbs.read() >> 3));
+        
+        if (debug == true)
+        {
+          System.out.println("Decoding time [ms]: "+(after-before)/(iter*1000000));
+          System.out.println("Read: "+(dbs.read() >> 3));
+        }
     }
 
 
-    private static void decode_(BitStream dbs, int[] data, DWT_CDF_9_7 dwt, EntropyDecoder decoder)
+    private static void decode_(int[] data, DWT_CDF_9_7 dwt, EntropyDecoder decoder, int blockSize)
     {
         // Block decoding
-        int dimImage = dwt.getDimension();
-        IndexedByteArray buffer = new IndexedByteArray(new byte[dimImage*dimImage], 0);
-        BlockCodec bd = new BlockCodec();
+        IndexedByteArray buffer = new IndexedByteArray(new byte[dwt.getWidth()*dwt.getHeight()], 0);
+        BlockCodec bd = new BlockCodec(blockSize);
         int decoded = 0;
 
         do
@@ -293,20 +343,15 @@ public class TestPipeline
         }
         while (decoded > 0);
 
-
-        int DIM_BAND_LL = dwt.getDimensionBandLL();
-        int levels = 0;
-
-        for (int dim=DIM_BAND_LL; dim<dimImage; dim<<=1)
-            levels++;
-
+        int levels = dwt.getLevels();
+        
         // Dequantization
 
-        // Quantizers must be known by the filter !!!
+        // Quantizers must be known by the decoder !!!
         // TODO: transmit quantizers
         int[] quantizers = new int[levels+1];
-        quantizers[0] = 310;//169;
-        quantizers[1] = 40;//26;
+        quantizers[0] = Q0;
+        quantizers[1] = Q1;
 
         for (int i=2; i<quantizers.length; i++)
         {
@@ -315,11 +360,11 @@ public class TestPipeline
         }
 
         // Inverse quantization
-        WaveletBandFilter filter = new WaveletBandFilter(dimImage,
-                DIM_BAND_LL, levels, quantizers);
+        WaveletBandFilter filter = new WaveletBandFilter(dwt.getWidth(),
+                dwt.getHeight(), levels, quantizers);
 
         IndexedIntArray destination = new IndexedIntArray(data, 0);
-        IndexedIntArray source = new IndexedIntArray(new int[dimImage*dimImage], 0);
+        IndexedIntArray source = new IndexedIntArray(new int[dwt.getWidth()*dwt.getHeight()], 0);
 
         for (int i=0; i<buffer.index; i++)
            source.array[i] = (int) buffer.array[i];
