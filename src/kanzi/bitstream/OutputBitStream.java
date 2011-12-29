a992a65a5d0a9e5c6ce27b966b98ebc5a52f1671
fred <fred@PC>
2011-12-29 11:36:21 -0800

    Simplify code

--- src/kanzi/bitstream/OutputBitStream.java
+++ src/kanzi/bitstream/OutputBitStream.java
@@ -4,15 +4,14 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 you may obtain a copy of the License at
 
-                http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
-*/
-
+ */
 package kanzi.bitstream;
 
 import kanzi.BitStreamException;
@@ -22,178 +21,150 @@ import java.io.OutputStream;
 
 /*package*/ final class OutputBitStream
 {
-    private final OutputStream os;
-    private final byte[] buffer;
-    private int position;
-    private int bitIndex;
-    private byte current;
-    private boolean closed;
-    private long written;
+   private final OutputStream os;
+   private final byte[] buffer;
+   private int position;
+   private int bitIndex;
+   private boolean closed;
+   private long written;
 
 
-    OutputBitStream(OutputStream os, int bufferSize)
-    {
-        this.os = os;
-        this.buffer = new byte[bufferSize];
-        this.bitIndex = 7;
-    }
+   OutputBitStream(OutputStream os, int bufferSize)
+   {
+      this.os = os;
+      this.buffer = new byte[bufferSize];
+      this.bitIndex = 7;
+   }
 
 
-    // Processes the least significant bit of the input integer
-    public synchronized boolean writeBit(int bit)
-    {
-        if (this.closed == true)
-            throw new BitStreamException("Stream closed", BitStreamException.STREAM_CLOSED);
+   // Processes the least significant bit of the input integer
+   public synchronized boolean writeBit(int bit)
+   {
+      if (this.closed == true)
+         throw new BitStreamException("Stream closed", BitStreamException.STREAM_CLOSED);
 
-        // --- START writeBit 'Macro'
-        this.current |= ((bit & 1) << this.bitIndex);
+      this.buffer[this.position] |= ((bit & 1) << this.bitIndex);
 
-        if (this.bitIndex == 0)
-        {
-            this.buffer[this.position] = this.current;
+      if (this.bitIndex == 0)
+        this.buffer[++this.position] = 0;
+
+      this.bitIndex = (this.bitIndex + 7) & 7;
+      this.written++;
+      return true;
+   }
+
+
+   // 'length' must be max 64
+   public synchronized int writeBits(long value, int length)
+   {
+      if (this.closed == true)
+         throw new BitStreamException("Stream closed", BitStreamException.STREAM_CLOSED);
+
+      if (this.position >= this.buffer.length - 8)
+         this.flush();
+
+      int remaining = length;
+
+      // Pad the current position in buffer
+      if (this.bitIndex != 7)
+      {
+         int idx = this.bitIndex;
+         final int len = (remaining <= idx + 1) ? remaining : idx + 1;
+         remaining -= len;
+         final int bits = (int) ((value >> remaining) & ((1 << len) - 1));
+         this.buffer[this.position] |= (bits << (idx + 1 - len));
+         idx = (idx + 8 - len) & 7;
+         this.written += len;
+         this.bitIndex = idx;
+
+         if (idx == 7)
             this.position++;
-            this.current = 0;
-        }
+      }
 
-        this.bitIndex = (this.bitIndex + 7) & 7;
-        this.written++;
-        // --- END writeBit 'Macro'
-
-        return true;
-    }
-
-
-    // 'length' must be max 64
-    public synchronized int writeBits(long bits, int length)
-    {
-        if (this.closed == true)
-            throw new BitStreamException("Stream closed", BitStreamException.STREAM_CLOSED);
-
-        if (this.position >= this.buffer.length-8)
-        {
-            try
-            {
-                this.flush();
-            }
-            catch (IOException e)
-            {
-                throw new BitStreamException(e.getClass().getName(), BitStreamException.INPUT_OUTPUT);
-            }
-        }
-
-        int remaining = length;
-
-        // Process the first bits one by one to pad this.current
-        while ((remaining > 0) && (this.bitIndex != 7))
-        {
-            remaining--;
-
-            // --- START writeBit 'Macro'
-            int bit = (int) ((bits >> remaining) & 1);
-            this.current |= ((bit & 1) << this.bitIndex);
-
-            if (this.bitIndex == 0)
-            {
-                this.buffer[this.position] = this.current;
-                this.position++;
-                this.current = 0;
-            }
-
-            this.bitIndex = (this.bitIndex + 7) & 7;
-            this.written++;
-            // --- END writeBit 'Macro'
-        }
-
-        // If this.current has been completely filled, use a fast path
-        if (this.bitIndex == 7)
-        {
-            // Progress byte by byte
-            while (remaining >= 8)
-            {
-                remaining -= 8;
-                this.buffer[this.position] = (byte) ((bits >> remaining) & 0xFF);
-                this.position++;
-                this.written += 8;
-            }
-
-            // Process the last bits one by one to update this.current
-            while (remaining > 0)
-            {
-                remaining--;
-
-                // --- START writeBit 'Macro'
-                int bit = (int) ((bits >> remaining) & 1);
-                this.current |= ((bit & 1) << this.bitIndex);
-
-                if (this.bitIndex == 0)
-                {
-                    this.buffer[this.position] = this.current;
-                    this.position++;
-                    this.current = 0;
-                }
-
-                this.bitIndex = (this.bitIndex + 7) & 7;
-                this.written++;
-                // --- END writeBit 'Macro'
-            }
-        }
-
-        return length;
-    }
-
-
-    public synchronized void flush() throws IOException
-    {
-        if (this.position > 0)
-           this.os.write(this.buffer, 0, this.position);
-
-        this.os.flush();
-        this.position = 0;
-    }
-
-
-    public synchronized void close() throws IOException
-    {
-        if (this.isClosed() == true)
-            return;
-
-        if ((this.written > 0) && (this.bitIndex != 7))
-        {
-            this.buffer[this.position] = this.current;
-            this.position++;
-            this.written -= (7 - this.bitIndex);
+      // Use a fast path
+      if (this.bitIndex == 7)
+      {
+         // Progress byte by byte
+         while (remaining >= 8)
+         {
+            remaining -= 8;
+            this.buffer[this.position++] = (byte) ((value >> remaining) & 0xFF);
             this.written += 8;
-            this.bitIndex = 7;
-        }
+         }
 
-        // Adjust stream size to multiple of 8 bytes (to allow decoding of long)
-        while ((this.written & 63) != 0)
-        {
-            if (this.position >= this.buffer.length)
-                this.flush();
+         // Process remaining bits
+         if (remaining > 0)
+         {
+            final int bits = (int) (value & ((1 << remaining) - 1));
+            this.buffer[this.position] |= (bits << (8 - remaining));
+            this.written += remaining;
+            this.bitIndex = (15 - remaining) & 7;
+         }
+      }
 
-            // Pad with 0xFF (do not choose 0x00)
-            this.buffer[this.position] = (byte) 0xFF;
-            this.position++;
-            this.written += 8;
-        }
-
-        this.flush();
-        this.current = 0;
-        this.closed = true;
-        this.os.close();
-    }
+      return length;
+   }
 
 
-    public synchronized long written()
-    {
-        return this.written;
-    }
+   public synchronized void flush() throws BitStreamException
+   {
+      try
+      {
+         if (this.position > 0)
+            this.os.write(this.buffer, 0, this.position);
+
+         this.os.flush();
+         this.position = 0;
+
+         for (int i=0; i<this.buffer.length; i++)
+            this.buffer[i] = 0;
+      }
+      catch (IOException e)
+      {
+         throw new BitStreamException(e.getClass().getName(), BitStreamException.INPUT_OUTPUT);
+      }
+   }
 
 
-    protected synchronized boolean isClosed()
-    {
-        return this.closed;
-    }
+   public synchronized void close() throws IOException
+   {
+      if (this.isClosed() == true)
+         return;
+
+      if ((this.written > 0) && (this.bitIndex != 7))
+      {
+         this.position++;
+         this.written -= (7 - this.bitIndex);
+         this.written += 8;
+         this.bitIndex = 7;
+      }
+
+      // Adjust stream size to multiple of 8 bytes (to allow decoding of long)
+      while ((this.written & 63) != 0)
+      {
+         if (this.position >= this.buffer.length)
+            this.flush();
+
+         // Pad with 0xFF (do not choose 0x00)
+         this.buffer[this.position] = (byte) 0xFF;
+         this.position++;
+         this.written += 8;
+      }
+
+      this.flush();
+      this.closed = true;
+      this.os.close();
+   }
+
+
+   public synchronized long written()
+   {
+      return this.written;
+   }
+
+
+   protected synchronized boolean isClosed()
+   {
+      return this.closed;
+   }
 }
-
