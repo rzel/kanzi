50c6adcb6b4a748d461affc11342e43a7f48fe75
fred <fred@PC>
2012-02-10 15:10:52 -0800

    Fix position of light Improve calculation of intensities Fix calculation of first/last line in normal map

--- src/kanzi/filter/LightingEffect.java
+++ src/kanzi/filter/LightingEffect.java
@@ -15,7 +15,7 @@ limitations under the License.
 
 package kanzi.filter;
 
-import java.util.Arrays;
+import kanzi.Global;
 import kanzi.VideoEffectWithOffset;
 
 
@@ -27,8 +27,6 @@ public class LightingEffect implements VideoEffectWithOffset
     private final int stride;
     private int offset;
     private int savedOffset;
-    private final int minIntensity;
-    private final int maxIntensity;
     private final int[] normalXY;
     private final int[] distanceMap;
     private int lightX;
@@ -39,46 +37,46 @@ public class LightingEffect implements VideoEffectWithOffset
    
     public LightingEffect(int width, int height, int lightX, int lightY, int radius, boolean bumpMapping)
     {
-       this(width, height, 0, width, lightX, lightY, radius, 0, 100, bumpMapping);
+       this(width, height, 0, width, lightX, lightY, radius, 100, bumpMapping);
     }
 
 
     // power in % (max pixel intensity)
     public LightingEffect(int width, int height, int offset, int stride, int lightX, int lightY, int radius,
-            int minIntensity, int power, boolean bumpMapping)
+             int power, boolean bumpMapping)
     {
         if (height < 8)
             throw new IllegalArgumentException("The height must be at least 8");
         
         if (width < 8)
             throw new IllegalArgumentException("The width must be at least 8");
+       
+        if (offset < 0)
+            throw new IllegalArgumentException("The offset must be at least 0");
+        
+        if (stride < 8)
+            throw new IllegalArgumentException("The stride must be at least 8");
 
-        if ((minIntensity < 0) || (minIntensity > 255))
-            throw new IllegalArgumentException("TThe minimum pixel intensity must be in [0.255]");
+        if ((power < 0) || (power > 1000))
+            throw new IllegalArgumentException("The power must be in the range [0..1000]");
 
         this.lightX = lightX;
         this.lightY = lightY;
         this.radius = radius;
         this.width  = width;
         this.height = height;
-        this.minIntensity = minIntensity;
-        this.maxIntensity = (255*power/100) > minIntensity ? (255*power/100) : minIntensity;
         this.bumpMapping = bumpMapping;
         this.stride = stride;
         this.offset = offset;
         this.savedOffset = offset-1;
         this.distanceMap = new int[radius*radius];
-        
-        if (this.bumpMapping == true)
-            this.normalXY = new int[width*height];
-        else
-            this.normalXY = null;
+        this.normalXY = (this.bumpMapping == true) ? new int[width*height] : null;
         
         // Initialize the distance table
         final int rd = this.radius;
-        final int top = 1 << 16;
-        final int invR = top / rd;
-        
+        final int top = 1 << 10;
+        final int maxIntensity = (255*power) / 100;
+      
         for (int y=0; y<rd; y++)
         {
             final int y2 = y * y;
@@ -86,14 +84,15 @@ public class LightingEffect implements VideoEffectWithOffset
             
             for (int x=0; x<rd; x++)
             {
-                int d = top - (int) (Math.sqrt(x*x+y2) * invR);
-                d = (this.maxIntensity * d) >> 16;
-                d = (d > this.minIntensity) ? d : this.minIntensity;
+                // 1 - alpha * (sqrt((x*x+y*y)/2)/(rd-1))
+                // alpha is used to cutoff before the end of the window (set to 5/4)
+                int d = top - (5 * (Global.sqrt((x*x+y2)/2) / (rd-1)) >> 2);
+                d = (maxIntensity * d) >> 10;
+                d = (d > 0) ? d : 0;
                 d = (d < 255) ? d : 255;
-                this.distanceMap[startLine+x] = d;
+                this.distanceMap[startLine+x] = d;          
             }
         }
-        
     }
         
     
@@ -113,6 +112,7 @@ public class LightingEffect implements VideoEffectWithOffset
 
         final int[] map = this.heightMap;
         final int w = this.width;
+        final int[] normals = this.normalXY;
 
         for (int j=0; j<this.height; j++)
         {
@@ -130,8 +130,12 @@ public class LightingEffect implements VideoEffectWithOffset
         }
         
         // First and last lines
-        Arrays.fill(this.normalXY, 0, w, 0);
-        Arrays.fill(this.normalXY, length-w-1, length-1, 0);
+        for (int i=0; i<w; i++)
+        {
+           normals[i] = 0;
+           normals[length-1-i] = 0;
+        }
+
         final int hh = this.height - 1;
         final int ww = this.width - 1;
         int offs = this.width;
@@ -140,7 +144,7 @@ public class LightingEffect implements VideoEffectWithOffset
         {
             // First in line (normalX = 0)
             int delta = map[offs+w] - map[offs-w];
-            this.normalXY[offs] = delta & 0xFFFF;
+            normals[offs] = delta & 0xFFFF;
             offs++;
             
             for (int x=1; x<ww; x++, offs++)
@@ -149,12 +153,12 @@ public class LightingEffect implements VideoEffectWithOffset
                 delta = map[offs+1] - map[offs-1];
                 final int tmp = (delta & 0xFFFF) << 16;
                 delta = map[offs+w] - map[offs-w];
-                this.normalXY[offs] = tmp | (delta & 0xFFFF);
+                normals[offs] = tmp | (delta & 0xFFFF);
             }
             
             // Last in line (normalX = 0)
             delta = map[offs+w] - map[offs-w];
-            this.normalXY[offs] = delta & 0xFFFF;
+            normals[offs] = delta & 0xFFFF;
             offs++;
         }
     }
@@ -163,91 +167,144 @@ public class LightingEffect implements VideoEffectWithOffset
     @Override
     public int[] apply(int[] src, int[] dst)
     {
-        int x0 = (this.lightX >= this.radius) ? this.lightX - this.radius : 0;
-        int x1 = (this.lightX + this.radius) < this.width ? this.lightX + this.radius : this.width;
-        int y0 = (this.lightY >= this.radius) ? this.lightY - this.radius : 0;
-        int y1 = (this.lightY + this.radius) < this.height ? this.lightY + this.radius : this.height;
+        final int lx = this.lightX;
+        final int ly = this.lightY;
+        int lineStart = this.offset + (lx - this.radius) + this.stride * (ly - this.radius);
+        final int x0 = (lx >= this.radius) ? lx - this.radius : 0;
+        final int x1 = (lx + this.radius) < this.width ? lx + this.radius : this.width;
+        final int y0 = (ly >= this.radius) ? ly - this.radius : 0;
+        final int y1 = (ly + this.radius) < this.height ? ly + this.radius : this.height;
         final int[] normals = this.normalXY;
         final int[] intensities = this.distanceMap;
         final int rd = this.radius;
+        final int w = this.width;
+        final int h = this.height;
         
+        if (y0 > 0)
+        {
+           // First lines are black
+           int offs = this.offset; 
+           
+           for (int yy=0; yy<y0; yy++)
+           {
+              for (int xx=0; xx<w; xx++)
+                 dst[offs+xx] = 0;
+
+              offs += this.stride;
+           }
+        }
+        
+        if (y1 < h)
+        {
+           // Last lines are black
+           int offs = this.offset + this.stride*h; 
+
+           for (int yy=h; yy>=y1; yy--)
+           {
+              for (int xx=0; xx<w; xx++)
+                 dst[offs+xx] = 0;
+
+              offs -= this.stride;
+           }
+        }
+       
         // Is there a bump mapping effect ?
         if (this.bumpMapping == true)
         {
             this.calculateNormalMap(src);
-            int dstStart = this.offset;
-            int srcStart = this.offset;
             
             for (int y=y0; y<y1; y++)
             {
-                int dstIdx = dstStart;
-                int srcIdx = srcStart;
+                int idx = lineStart-x0;
+                int offs = y * w;
                 
+                if (x0 > 0)
+                {
+                   // First pixels
+                   for (int xx=0; xx<x0; xx++)
+                      dst[idx++] = 0;
+                }        
+               
                 for (int x=x0; x<x1; x++)
                 {
-                    final int normal = normals[y*this.width+x];
+                    final int normal = normals[offs+x];
                     
                     // First, extract the normal X coord. (16 upper bits) out of normalXY
                     // Use a short first, then expand to an int (takes care of negative
                     // number expansion)
                     short tmp = (short) (normal >> 16);
-                    int val = tmp - x + this.lightX;
+                    int val = tmp - x + lx;
                     int dx = (val > 0) ? val : -val;
                     dx = (dx < rd) ? dx : rd-1;
                     
                     // Extract the normal Y coord. as a short then expand to an int
                     // (takes care of negative number expansion)
                     tmp = (short) (normal & 0xFFFF);
-                    val = tmp - y + this.lightY;
+                    val = tmp - y + ly;
                     int dy = (val > 0) ? val : -val;
                     dy = (dy < rd) ? dy : rd-1;
                     
                     final int intensity = intensities[dy*rd+dx];
-                    final int pixel = src[srcIdx++];
-                    int r = (pixel >> 16) & 0xFF;
-                    int g = (pixel >>  8) & 0xFF;
-                    int b =  pixel & 0xFF;
-                    r = (intensity * r) >> 8;
-                    g = (intensity * g) >> 8;
-                    b = (intensity * b) >> 8;
-                    
-                    dst[dstIdx++] = (r << 16) | (g << 8) | b;
-                }
-
-                dstStart += this.stride;
-                srcStart += this.stride;
-            }
-        }
-        else // No bump mapping: just lighting
-        {
-            int dstStart = this.offset;
-            int srcStart = this.offset;
-            
-            for (int y=y0; y<y1; y++)
-            {
-                int dstIdx = dstStart;
-                int srcIdx = srcStart;
-                int dy = (y > this.lightY) ? y - this.lightY : this.lightY - y;
-                dy = (dy < rd) ? dy : rd - 1;
-                final int yy = dy * rd;
-                
-                for (int x=x0; x<x1; x++)
-                {
-                    int dx = (x > this.lightX) ? x - this.lightX : this.lightX - x;
-                    dx = (dx < rd) ? dx : rd - 1;
-                    final int intensity = intensities[yy+dx];
-                    final int pixel = src[srcIdx++];
+                    final int pixel = src[idx];
                     int r = (pixel >> 16) & 0xFF;
                     int g = (pixel >>  8) & 0xFF;
                     int b =  pixel & 0xFF;
                     r = (intensity * r) >> 8;
                     g = (intensity * g) >> 8;
                     b = (intensity * b) >> 8;                    
-                    dst[dstIdx++] = (r << 16) | (g << 8) | b;
+                    dst[idx++] = (r << 16) | (g << 8) | b;
                 }
 
-                dstStart += this.stride;
-                srcStart += this.stride;
+                if (x1 < w)
+                {
+                   // First pixels
+                   for (int xx=x1; xx<w; xx++)
+                      dst[idx++] = 0;
+                }
+
+                lineStart += this.stride;
+            }
+        }
+        else // No bump mapping: just lighting
+        {            
+            for (int y=y0; y<y1; y++)
+            {
+                int idx = lineStart-x0;
+                
+                if (x0 > 0)
+                {
+                   // First pixels
+                   for (int xx=0; xx<x0; xx++)
+                      dst[idx++] = 0;
+                }        
+
+                int dy = (y > ly) ? y - ly : ly - y;
+                dy = (dy < rd) ? dy : rd - 1;
+                final int yy = dy * rd;
+                
+                for (int x=x0; x<x1; x++)
+                {
+                    int dx = (x > lx) ? x - lx : lx - x;
+                    dx = (dx < rd) ? dx : rd - 1;
+                    final int intensity = intensities[yy+dx];
+                    final int pixel = src[idx];
+                    int r = (pixel >> 16) & 0xFF;
+                    int g = (pixel >>  8) & 0xFF;
+                    int b =  pixel & 0xFF;
+                    r = (intensity * r) >> 8;
+                    g = (intensity * g) >> 8;
+                    b = (intensity * b) >> 8;                    
+                    dst[idx++] = (r << 16) | (g << 8) | b;
+                }
+
+                if (x1 < w)
+                {
+                   // First pixels
+                   for (int xx=x1; xx<w; xx++)
+                      dst[idx++] = 0;
+                }
+
+                lineStart += this.stride;
             }
         }
         
