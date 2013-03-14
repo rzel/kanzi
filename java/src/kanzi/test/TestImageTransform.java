fb400447162aba01b6aa7da4162ee040863c73f7
fred <fred@PC>
2013-03-14 14:03:49 -0700

    Test more transform sizes

--- java/src/kanzi/test/TestImageTransform.java
+++ java/src/kanzi/test/TestImageTransform.java
@@ -17,6 +17,11 @@ package kanzi.test;
 
 import kanzi.transform.DCT8;
 import kanzi.IntTransform;
+import kanzi.transform.DCT16;
+import kanzi.transform.DCT32;
+import kanzi.transform.DCT4;
+import kanzi.transform.WHT16;
+import kanzi.transform.WHT32;
 import kanzi.transform.WHT4;
 import kanzi.transform.WHT8;
 import kanzi.util.ImageQualityMonitor;
@@ -28,8 +33,8 @@ public class TestImageTransform
         String filename = (args.length > 0) ? args[0] : "C:\\temp\\lena.jpg";
         javax.swing.ImageIcon icon = new javax.swing.ImageIcon(filename);
         java.awt.Image image = icon.getImage();
-        int w = image.getWidth(null) & 0xFFF8;
-        int h = image.getHeight(null) & 0xFFF8;
+        int w = image.getWidth(null) & 0xFFF0;
+        int h = image.getHeight(null) & 0xFFF0;
         java.awt.GraphicsDevice gs = java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getScreenDevices()[0];
         java.awt.GraphicsConfiguration gc = gs.getDefaultConfiguration();
         java.awt.image.BufferedImage img = gc.createCompatibleImage(w, h, java.awt.Transparency.OPAQUE);
@@ -50,19 +55,34 @@ public class TestImageTransform
         img.getRaster().setDataElements(0, 0, w, h, rgb);
 
         javax.swing.JFrame frame = new javax.swing.JFrame("Original");
-        frame.setBounds(200, 100, w, h);
+        frame.setBounds(100, 100, w, h);
         frame.add(new javax.swing.JLabel(new javax.swing.ImageIcon(img)));
         frame.setVisible(true);
 
-        DCT8 dct8 = new DCT8();
-        transform(dct8, w, h, rgb, 8, "Discrete Cosine Transform 8x8", 400, 200);
+        DCT4 dct4 = new DCT4();
+        transform(dct4, w, h, rgb, 4, "Discrete Cosine Transform 4x4", 150, 150);
 
-        WHT8 wht8 = new WHT8();
-        transform(wht8, w, h, rgb, 8, "Walsh-Hadamard Transform 8x8", 600, 300);
+        DCT8 dct8 = new DCT8();
+        transform(dct8, w, h, rgb, 8, "Discrete Cosine Transform 8x8", 200, 200);
+
+        DCT16 dct16 = new DCT16();
+        transform(dct16, w, h, rgb, 16, "Discrete Cosine Transform 16x16", 250, 250);
+
+        DCT32 dct32 = new DCT32();
+        transform(dct32, w, h, rgb, 32, "Discrete Cosine Transform 32x32", 300, 300);
 
         WHT4 wht4 = new WHT4();
-        transform(wht4, w, h, rgb, 4, "Walsh-Hadamard Transform 4x4", 800, 400);
+        transform(wht4, w, h, rgb, 4, "Walsh-Hadamard Transform 4x4", 350, 350);
         
+        WHT8 wht8 = new WHT8();
+        transform(wht8, w, h, rgb, 8, "Walsh-Hadamard Transform 8x8", 400, 400);
+
+        WHT16 wht16 = new WHT16();
+        transform(wht16, w, h, rgb, 16, "Walsh-Hadamard Transform 16x16", 450, 450);
+
+        WHT32 wht32 = new WHT32();
+        transform(wht32, w, h, rgb, 32, "Walsh-Hadamard Transform 32x32", 500, 500);
+
         try
         {
            Thread.sleep(25000);
@@ -80,7 +100,7 @@ public class TestImageTransform
   {
     int len = w * h;
     int[] rgb2 = new int[len];
-    int[] data = new int[64];
+    int[] data = new int[1024];
     long sum = 0L;
     int iter = 1000;
 
